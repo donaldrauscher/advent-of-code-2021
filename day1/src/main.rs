1f012aefb972b0c711bb7397dd9b81c255fd404e
Donald Rauscher <donald.rauscher@gmail.com>
2022-07-03 13:06:00 -0500

    Pattern matching/option for first_star; argument for second_star

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,46 +2,45 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-// first star
-// fn main() {
-//
-//     let mut last_measurement: i32 = -1;
-//     let mut num_increases: i32 = 0;
-//
-//     let lines = read_lines("./input.txt");
-//     for line in lines {
-//         if let Ok(measurement) = line {
-//             let measurement: i32 = measurement.trim().parse().unwrap();
-//
-//             if last_measurement > 0 {
-//                 if measurement > last_measurement {
-//                     num_increases += 1;
-//                 }
-//             }
-//
-//             last_measurement = measurement;
-//         }
-//     }
-//
-//     println!("Number of measurement increases: {}", num_increases);
-//
-// }
 
-// second star
-fn main() {
+fn first_star() {
 
-    let mut last_measurements: Vec<i32> = Vec::with_capacity(4);
-    let mut num_increases: i32 = 0;
+    let mut last_measurement: Option<u32> = None;
+    let mut num_increases: u32 = 0;
 
     let lines = read_lines("./input.txt");
     for line in lines {
         if let Ok(measurement) = line {
-            let measurement: i32 = measurement.trim().parse().unwrap();
+            let measurement: u32 = measurement.trim().parse().unwrap();
+
+            num_increases += match last_measurement {
+                None => 0,
+                Some(m) => if measurement > m { 1 } else { 0 }
+            };
+
+            last_measurement = Some(measurement);
+        }
+    }
+
+    println!("Number of measurement increases: {}", num_increases);
+
+}
+
+
+fn second_star(rolling_window: usize) {
+
+    let mut last_measurements: Vec<u32> = Vec::with_capacity(rolling_window + 1);
+    let mut num_increases: u32 = 0;
+
+    let lines = read_lines("./input.txt");
+    for line in lines {
+        if let Ok(measurement) = line {
+            let measurement: u32 = measurement.trim().parse().unwrap();
 
             last_measurements.push(measurement);
 
-            if last_measurements.len() == 4 {
-                let comp1 = vector_sum(&last_measurements[..3]);
+            if last_measurements.len() == (rolling_window + 1) {
+                let comp1 = vector_sum(&last_measurements[..rolling_window]);
                 let comp2 = vector_sum(&last_measurements[1..]);
                 if comp2 > comp1 {
                     num_increases += 1;
@@ -52,13 +51,22 @@ fn main() {
         }
     }
 
-    println!("Number of measurement increases: {}", num_increases);
+    println!("Number of measurement increases with rolling window of {}: {}",
+        rolling_window, num_increases);
 
 }
 
 
-fn vector_sum(v: &[i32]) -> i32 {
-    let mut sum: i32 = 0;
+fn main() {
+    first_star();
+    second_star(3);
+    // this should match first_star()
+    second_star(1);
+}
+
+
+fn vector_sum(v: &[u32]) -> u32 {
+    let mut sum: u32 = 0;
     for vv in v {
         sum += vv;
     }
