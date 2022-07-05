12fc5bfe40a693df94c31a9cb2378428fbc627fe
Donald Rauscher <donald.rauscher@gmail.com>
2022-07-04 23:07:35 -0500

    Adding game play

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -5,23 +5,16 @@ use std::path::Path;
 #[derive(Debug)]
 struct Board {
     numbers: Vec<Vec<u32>>,
-    picked: Vec<Vec<bool>>
+    picked: Vec<Vec<bool>>,
+    won: bool
 }
 
 impl Board {
-    fn parse(input: &Vec<String>) -> Board {
-        let mut numbers: Vec<Vec<u32>> = Vec::with_capacity(5);
-        for i in input {
-            let mut row = Vec::with_capacity(5);
-            for j in 0..5 {
-                row.push(i[(j*3)..(j*3+2)].trim().parse().unwrap());
-            }
-            numbers.push(row);
-        }
-
+    fn create(numbers: Vec<Vec<u32>>) -> Board {
         Board {
             numbers: numbers,
-            picked: vec![vec![false; 5]; 5]
+            picked: vec![vec![false; 5]; 5],
+            won: false
         }
     }
 
@@ -30,9 +23,26 @@ impl Board {
             for col in 0..5 {
                 if self.numbers[row][col] == *number {
                     self.picked[row][col] = true;
+                    self.won = self.has_won(row, col);
                 }
             }
         }
+    }
+
+    fn has_won(&self, row: usize, col: usize) -> bool {
+        let mut row_count: u32 = 0;
+        for c in 0..5 {
+            if self.picked[row][c] {
+                row_count += 1;
+            }
+        }
+        let mut col_count: u32 = 0;
+        for r in 0..5 {
+            if self.picked[r][col] {
+                col_count += 1;
+            }
+        }
+        return (row_count == 5) || (col_count == 5);
     }
 }
 
@@ -41,9 +51,9 @@ fn main() {
     let mut line_number: u32 = 0;
 
     let mut pick_numbers: Vec<u32> = Vec::new();
+    let mut board_numbers: Vec<Vec<u32>> = Vec::with_capacity(5);
     let mut boards: Vec<Board> = Vec::new();
 
-    let mut board_numbers_tmp: Vec<String> = Vec::with_capacity(5);
     for line in lines {
         if let Ok(line_text) = line {
             if line_number == 0 {
@@ -51,22 +61,33 @@ fn main() {
             } else if line_text == "" {
                 continue;
             } else {
-                board_numbers_tmp.push(line_text);
+                board_numbers.push(parse_board_numbers(&line_text));
             }
             // time to create a new board!
-            if board_numbers_tmp.len() == 5 {
-                let board: Board = Board::parse(&board_numbers_tmp);
+            if board_numbers.len() == 5 {
+                let board: Board = Board::create(board_numbers);
                 boards.push(board);
-                board_numbers_tmp.clear();
+                board_numbers = Vec::with_capacity(5);
             }
         }
         line_number += 1;
     }
 
-    println!("{:?}", pick_numbers);
-    println!("{:?}", boards[0]);
-    boards[0].pick_number(&48);
-    println!("{:?}", boards[0]);
+    let mut winning_board: Option<usize> = None;
+    'outer: for n in pick_numbers {
+        println!("Number {}...", n);
+        for i in 0..boards.len() {
+            let board = &mut boards[i];
+            board.pick_number(&n);
+            if board.won {
+                winning_board = Some(i);
+                break 'outer;
+            }
+        }
+    }
+    if let Some(b) = winning_board {
+        println!("{:?}", boards[b]);
+    }
 }
 
 fn parse_pick_numbers(line_text: &str, pick_numbers: &mut Vec<u32>) {
@@ -74,6 +95,14 @@ fn parse_pick_numbers(line_text: &str, pick_numbers: &mut Vec<u32>) {
     for p in pick_numbers_str {
         pick_numbers.push(p.trim().parse().unwrap());
     }
+}
+
+fn parse_board_numbers(line_text: &str) -> Vec<u32> {
+    let mut row = Vec::with_capacity(5);
+    for i in 0..5 {
+        row.push(line_text[(i*3)..(i*3+2)].trim().parse().unwrap());
+    }
+    return row;
 }
 
 fn read_lines<P>(filename: P) -> io::Lines<io::BufReader<File>>
