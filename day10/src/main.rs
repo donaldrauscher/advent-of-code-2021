6d37c832640c574d945a9bca7581be6aa5d805d2
Donald Rauscher <donald.rauscher@gmail.com>
2022-07-17 22:54:34 -0500

    Add autocompletion score (second star)

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -8,7 +8,7 @@ enum ChunkChar {
 
 #[derive(Debug)]
 enum LineStatus {
-    Incomplete,
+    Incomplete(Vec<char>),
     Corrupted(char)
 }
 
@@ -31,17 +31,44 @@ fn main() {
                     '>' => 25137,
                     _ => unreachable!()
                 },
-                LineStatus::Incomplete => 0
+                LineStatus::Incomplete(_) => 0
             }
         })
         .sum::<usize>();
 
-    println!("Total error score: {}", error_score);
+    let mut autocomplete_score = include_str!("../input.txt")
+        .lines()
+        .map(|line| check_line(line, &open_close_match))
+        .filter_map(|status| {
+            match status {
+                LineStatus::Corrupted(_) => None,
+                LineStatus::Incomplete(c) => {
+                    Some(c
+                        .iter()
+                        .fold(0, |total, cc| {
+                            total*5 + match cc {
+                                ')' => 1,
+                                ']' => 2,
+                                '}' => 3,
+                                '>' => 4,
+                                _ => unreachable!()
+                            }
+                        }))
+                }
+            }
+        })
+        .collect::<Vec<usize>>();
+
+    autocomplete_score.sort();
+    let auto_complete_score_mid = autocomplete_score[autocomplete_score.len()/2];
+
+    println!("Total error score: {:?}", error_score);
+    println!("Middle autocompletion score: {:?}", auto_complete_score_mid);
 }
 
 fn check_line(line: &str, open_close_match: &HashMap<char,char>) -> LineStatus {
     let mut open_chunks: Vec<char> = Vec::new();
-    let mut line_status: LineStatus = LineStatus::Incomplete;
+    let mut line_status: Option<LineStatus> = None;
 
     let chars = line
         .chars()
@@ -63,12 +90,20 @@ fn check_line(line: &str, open_close_match: &HashMap<char,char>) -> LineStatus {
                 if i == correct_close {
                     open_chunks.pop();
                 } else {
-                    line_status = LineStatus::Corrupted(i);
+                    line_status = Some(LineStatus::Corrupted(i));
                     break;
                 }
             }
         }
     }
 
-    return line_status;
+    if let Some(ls) = line_status {
+        return ls;
+    } else {
+        let mut completion_chars: Vec<char> = Vec::new();
+        while let Some(c) = open_chunks.pop() {
+            completion_chars.push(open_close_match[&c]);
+        }
+        return LineStatus::Incomplete(completion_chars);
+    }
 }
