708472eb7bc6764c5457b32b650121b56e6063a8
Donald Rauscher <donald.rauscher@gmail.com>
2022-11-12 12:21:03 -0600

    Switch to using priority queue for efficiency

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,56 +1,74 @@
 
-fn get_neighbors(node: usize, r: usize, c: usize) -> Vec<usize> {
-    let x = node % c;
-    let y = node / c;
-    let mut neighbors: Vec<usize> = Vec::new();
-    if x > 0 {
-        neighbors.push(y*c + x - 1);
-    }
-    if y > 0 {
-        neighbors.push((y-1)*c + x);
-    }
-    if x < (c - 1) {
-        neighbors.push(y*c + x + 1);
-    }
-    if y < (r - 1) {
-        neighbors.push((y+1)*c + x);
-    }
-    return neighbors;
+use std::hash::{Hash, Hasher};
+use priority_queue::PriorityQueue;
+
+
+#[derive(Debug)]
+struct Node {
+    location: usize,
+    distance: isize
 }
+
+impl Node {
+    fn add_neighbors(&self, pq: &mut PriorityQueue<Node, isize>, risk_levels: &Vec<isize>, r: usize, c: usize) {
+        let x = self.location % c;
+        let y = self.location / c;
+        if x < (c - 1) {
+            let n = y*c + x + 1;
+            let d = self.distance + risk_levels[n];
+            pq.push_increase(Node { location: n, distance: d }, -d);
+        }
+        if y < (r - 1) {
+            let n = (y+1)*c + x;
+            let d = self.distance + risk_levels[n];
+            pq.push_increase(Node { location: n, distance: d }, -d);
+        }
+    }
+}
+
+impl Hash for Node {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.location.hash(state);
+    }
+}
+
+impl PartialEq for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.location == other.location
+    }
+}
+impl Eq for Node {}
+
 
 fn main() {
     // load data
     let n_row = include_str!("../input.txt").lines().count();
     let n_col = include_str!("../input.txt").split_once("\n").unwrap().0.chars().count();
-    let risk: Vec<usize> = include_str!("../input.txt")
+    let risk_levels: Vec<isize> = include_str!("../input.txt")
         .lines()
         .flat_map(|r| {
             r
                 .chars()
-                .map(|c| c.to_string().parse::<usize>().unwrap())
-                .collect::<Vec<usize>>()
+                .map(|c| c.to_string().parse::<isize>().unwrap())
+                .collect::<Vec<isize>>()
         })
         .collect::<Vec<_>>();
 
-    // // initialize djikstras
-    let start = 0;
-    let end = n_row*n_col - 1;
-    let mut dist: Vec<usize> = vec![usize::max_value(); n_row*n_col];
-    dist[start] = 0;
+    // insert origin
+    let origin = Node{ location: 0, distance: 0 };
+    let mut pq = PriorityQueue::new();
+    pq.push(origin, 0);
 
     // run dijikstras
-    let mut unvisited: Vec<usize> = vec![0];
-
-    while unvisited.len() > 0 {
-        let n = unvisited.remove(0);
-        for nn in get_neighbors(n, n_row, n_col) {
-            if (dist[n] + risk[nn]) < dist[nn] {
-                dist[nn] = dist[n] + risk[nn];
-                if !unvisited.contains(&nn) {
-                    unvisited.push(nn);
-                }
-            }
+    let mut distance: Option<isize> = None;
+    while let Some((node, _)) = pq.pop() {
+        if node.location == (n_row * n_col - 1) {
+            distance = Some(node.distance);
+            break;
         }
+        node.add_neighbors(&mut pq, &risk_levels, n_row, n_col);
     }
-    println!("Minimum path to destination: {}", dist[end]);
+    if let Some(d) = distance {
+        println!("Distance: {:?}", d);
+    }
 }
