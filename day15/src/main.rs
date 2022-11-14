a9cd8b69a217a5124954a61260ab58ab77d44cbb
Donald Rauscher <donald.rauscher@gmail.com>
2022-11-13 20:23:59 -0600

    Add visited hash set for big speed improvement

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,5 +1,6 @@
 
 use std::hash::{Hash, Hasher};
+use std::collections::HashSet;
 use priority_queue::PriorityQueue;
 
 
@@ -10,28 +11,41 @@ struct Node {
 }
 
 impl Node {
-    fn add_neighbors(&self, pq: &mut PriorityQueue<Node, isize>, risk_levels: &Vec<isize>, r: usize, c: usize) {
+    fn add_neighbors(&self,
+                     pq: &mut PriorityQueue<Node, isize>,
+                     visited: &HashSet<Node>,
+                     risk_levels: &Vec<isize>,
+                     r: usize,
+                     c: usize) {
         let x = self.location % c;
         let y = self.location / c;
         if x > 0 {
             let n = y*c + x - 1;
-            let d = self.distance + risk_levels[n];
-            pq.push_increase(Node { location: n, distance: d }, -d);
+            self.add_neighbor(n, pq, visited, risk_levels);
         }
         if y > 0 {
             let n = (y-1)*c + x;
-            let d = self.distance + risk_levels[n];
-            pq.push_increase(Node { location: n, distance: d }, -d);
+            self.add_neighbor(n, pq, visited, risk_levels);
         }
         if x < (c - 1) {
             let n = y*c + x + 1;
-            let d = self.distance + risk_levels[n];
-            pq.push_increase(Node { location: n, distance: d }, -d);
+            self.add_neighbor(n, pq, visited, risk_levels);
         }
         if y < (r - 1) {
             let n = (y+1)*c + x;
-            let d = self.distance + risk_levels[n];
-            pq.push_increase(Node { location: n, distance: d }, -d);
+            self.add_neighbor(n, pq, visited, risk_levels);
+        }
+    }
+
+    fn add_neighbor(&self,
+                    n: usize,
+                    pq: &mut PriorityQueue<Node, isize>,
+                    visited: &HashSet<Node>,
+                    risk_levels: &Vec<isize>) {
+        let d = self.distance + risk_levels[n];
+        let node = Node { location: n, distance: d };
+        if !visited.contains(&node) {
+            pq.push_increase(node, -d);
         }
     }
 }
@@ -64,8 +78,29 @@ fn main() {
         })
         .collect::<Vec<_>>();
 
+    // // multiple board
+    // let n_row = n_row * 5;
+    // let n_col = n_col * 5;
+    // let risk_levels = (
+    //     (0..n_row*n_col*25)
+    //     .map(|l| {
+    //         let x = (l % (n_col * 5)) % 5;
+    //         let y = (l / (n_col * 5)) % 5;
+    //         risk_levels[y*n_col + x]
+    //     })
+    //     .(0..max_pos)
+    //         .map(|p| {
+    //             crabs
+    //                 .iter()
+    //                 .map(|x|{ let n = (x - p).abs(); n*(n+1)/2 })
+    //                 .sum::<i32>()
+    //         })
+    // )
+
     // set up priority queue
     let mut pq = PriorityQueue::new();
+    let mut visited: HashSet<Node> = HashSet::new();
+
     let origin = Node{ location: 0, distance: 0 };
     pq.push(origin, 0);
 
@@ -76,7 +111,8 @@ fn main() {
             distance = Some(node.distance);
             break;
         }
-        node.add_neighbors(&mut pq, &risk_levels, n_row, n_col);
+        node.add_neighbors(&mut pq, &visited, &risk_levels, n_row, n_col);
+        visited.insert(node);
     }
     if let Some(d) = distance {
         println!("Distance: {:?}", d);
